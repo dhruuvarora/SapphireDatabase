9a087174dd3d664cee7e5de23f16b34781aae066
Dhruv Arora <dhruv862arora@gmail.com>
2025-06-17 13:07:40 +0530

    feat: income-proof-type column

--- V8__Income_Proof.sql
+++ V8__Income_Proof.sql
@@ -1,3 +1,11 @@
+CREATE TYPE income_proof_type_enum AS ENUM (
+    'bank_statement_6m_10k',
+    'salary_slip_15k_monthly', 
+    'form_16_120k_annual',
+    'net_worth_certificate_10l',
+    'demat_statement_10k_holdings'
+);
+
 ALTER TABLE signup_checkpoints
     ADD COLUMN income_proof TEXT;
 
@@ -13,3 +21,9 @@ ALTER TABLE "user"
     ADD COLUMN esign               TEXT,
     ADD COLUMN pan_document        TEXT,
     ADD COLUMN pan_document_issuer VARCHAR(100);
+
+ALTER TABLE signup_checkpoints
+    ADD COLUMN income_proof_type income_proof_type_enum;
+
+ALTER TABLE "user"
+    ADD COLUMN income_proof_type income_proof_type_enum;
